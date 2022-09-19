607ba169489aae258b17c977808f1f52512a9282
qais-alsgher <qaisalsgher@gmail.com>
2022-09-19 15:13:35 +0000

    remove comment for test

--- __tests__/routesComment.test.js
+++ __tests__/routesComment.test.js
@@ -4,7 +4,7 @@ const request = supertest(server.app);
 jest.setTimeout(10000);
 
 
-xdescribe('test router get method', () => {
+describe('test router get method', () => {
     it('test get all', async () => {
         const res = await request.get('/comment');
         expect(res.status).toEqual(200);
@@ -17,7 +17,7 @@ xdescribe('test router get method', () => {
 
 });
 
-xdescribe('test router comment method', () => {
+describe('test router comment method', () => {
     it('test comment to create comment', async () => {
         const newcomment = {
             commentAouthr: 'abdullah',
@@ -29,7 +29,7 @@ xdescribe('test router comment method', () => {
     })
 });
 
-xdescribe('test router put method', () => {
+describe('test router put method', () => {
     it('test put to update comment', async () => {
         const updatecomment = {
             commentContent: 'Hi'
@@ -39,7 +39,7 @@ xdescribe('test router put method', () => {
     })
 });
 
-xdescribe('test router delet method', () => {
+describe('test router delet method', () => {
     it('test delete to delete comment', async () => {
         const res = await request.delete('/comment/12');
         expect(res.status).toEqual(204);
