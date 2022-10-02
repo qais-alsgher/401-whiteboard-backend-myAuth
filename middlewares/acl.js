07afd69ad1e42a1e7b2b2f4f55b561c819c04fbf
qais-alsgher <qaisalsgher@gmail.com>
2022-10-02 18:35:23 +0000

    fix err

--- middlewares/acl.js
+++ middlewares/acl.js
@@ -1,22 +1,27 @@
 
 `use strict`;
-const { Post } = require('../models');
+const { Post } = require('../models/index');
 
 const acl = async (req, res, next) => {
     const id = req.params.id;
-    const userName = req.user.userName
+
     const postFind = await Post.read(id);
+    try {
 
-    if (postFind.postAouthr === userName) {
-        next();
+        if (postFind.dataValues.postAouthr === req.user.userName) {
+            next();
+        } else if (!req.user.capabilities.includes('update')) {
+
+            next('You Are Not Authorize!!!');
+        } else {
+            next();
+        }
+
+    } catch (err) {
+        console.log(err.message);
+        next(err.message);
     }
 
-
-    if (!req.user.capabilities.includes('update')) {
-        next('You Are Not Authorize!!!');
-    }
-
-    next();
 
 };
 
