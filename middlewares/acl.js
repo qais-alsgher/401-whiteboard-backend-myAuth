d096b59d7fef20a4ad2b57e90e03566e36e82d43
qais-alsgher <qaisalsgher@gmail.com>
2022-10-02 14:12:11 +0000

    edite for middlewares acl if owner post can edit and delet post

--- middlewares/acl.js
+++ middlewares/acl.js
@@ -1,7 +1,15 @@
 
 `use strict`;
+const { Post } = require('../models');
 
-const acl = (req, res, next) => {
+const acl = async (req, res, next) => {
+    const id = req.params.id;
+
+    const postFind = await Post.read(id);
+    if (postFind.userName === req.body.userName) {
+        next();
+    }
+
     if (!req.user.capabilities.includes('update')) {
         next('You Are Not Authorize!!!');
     }
